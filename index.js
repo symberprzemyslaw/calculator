0b21043ceb05bee505455a3924814a0191849a5b
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-07-05 23:17:35 +0200

    almost working, need some fixes

--- index.js
+++ index.js
@@ -1,55 +1,50 @@
+let numA = '';
+let numB;
+let operand = '+';
 
-let numA = ''
-let numB 
-let operand = '+'
+const digits = document.querySelectorAll("button");
+const display = document.querySelector(".first-row-dis");
 
-const digits = document.querySelectorAll("button")
-const display = document.querySelector(".first-row-dis")
-
-///Pseudo-code
-/*
-Najpierw trzeba moc w ogole stworzyc string, pozniej wchodzi operand ktory 
-decyduje o calej operacji. Nastepnie klika sie = i pokazuje sie wynik operacji
-najlepiej zeby wyswietlal sie w dolnym rzedzie, ale to osobna logika.
-Powstanie stringa,wybranie operandu, drugi string, akcja.
-
-
-
-*/
-
-function operate(numA,numB,operand){
-    if(operand === "+"){
-       return add(numA,numB)
-    } else if (operand === "-"){
-        return sub(numA,numB)
-    } else if (operand === "*"){
-        return multi(numA,numB)
-    } else if (operand === "/"){
-        return div(numA,numB)
-    }
-    
+function operate(numA, numB, operand) {
+  if (operand === "+") {
+    return add(Number(numA), Number(numB));
+  } else if (operand === "-") {
+    return sub(Number(numA), Number(numB));
+  } else if (operand === "*") {
+    return multi(Number(numA), Number(numB));
+  } else if (operand === "/") {
+    return div(Number(numA), Number(numB));
+  }
 }
-//Basic operation functions
-const add = (a,b) => a + b
-const sub = (a,b) => a - b
-const multi = (a,b) => a * b
-const div = (a,b) => a / b
 
-let selectedNum = numA
-digits.forEach(element => element.addEventListener('click', e => {
-    let operand;
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const multi = (a, b) => a * b;
+const div = (a, b) => a / b;
+
+let selectedNum = numA;
+
+digits.forEach(element =>
+  element.addEventListener("click", e => {
     if (!element.id) {
-        selectedNum += element.innerText
-        display.textContent += selectedNum
-        console.log("nie mam id")
-    } else if (element.id === 'add-btn'){
-        operand = '+'
-        selectedNum = numB
-        display.textContent += ' + '
-    } else if (element.id === 'score-btn'){
-        display.textContent = operate(numA,numB,operand)
-    }
+      selectedNum += element.innerText;
+      display.textContent += element.innerText;
+    } else if (element.id === "clear-btn") {
+      numB = ''
+      numA = ''
+      display.textContent = '';
+      selectedNum = '';
+    } else if (element.id === "score-btn") {
+      numB = selectedNum;
+      const result = operate(numA, numB, operand);
+      display.textContent = result;
+      numA = result;
+      selectedNum = '';
+    } else if (element.id) {
+      operand = element.innerText;
+      numA = selectedNum;
+      selectedNum = '';
+      display.textContent += ` ${operand} `;
+  }})
 
-    
-}))
-
+);
