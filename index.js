3fc4a25bef18aab19e4d6aa251941a6e846e1a74
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-07-05 14:03:21 +0200

    Trying new branch

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 
-let numA;
-let numB;
-let operand;
+let numA = ''
+let numB 
+let operand = '+'
+
+const digits = document.querySelectorAll("button")
+const display = document.querySelector(".first-row-dis")
+
+///Pseudo-code
+/*
+Najpierw trzeba moc w ogole stworzyc string, pozniej wchodzi operand ktory 
+decyduje o calej operacji. Nastepnie klika sie = i pokazuje sie wynik operacji
+najlepiej zeby wyswietlal sie w dolnym rzedzie, ale to osobna logika.
+Powstanie stringa,wybranie operandu, drugi string, akcja.
+
+
+
+*/
 
 function operate(numA,numB,operand){
     if(operand === "+"){
-        add(numA,numB)
+       return add(numA,numB)
     } else if (operand === "-"){
-        sub(numA,numB)
+        return sub(numA,numB)
     } else if (operand === "*"){
-        multi(numA,numB)
+        return multi(numA,numB)
     } else if (operand === "/"){
-        div(numA,numB)
+        return div(numA,numB)
     }
     
 }
-
-
-
-
 //Basic operation functions
 const add = (a,b) => a + b
 const sub = (a,b) => a - b
 const multi = (a,b) => a * b
 const div = (a,b) => a / b
 
-console.log(add(2,3))
-
-const digits = document.querySelectorAll("button")
+let selectedNum = numA
 digits.forEach(element => element.addEventListener('click', e => {
-    let textInsideBtn = e.target.innerText
-    let textOnDisplay = document.querySelector('.display')
-    textOnDisplay.textContent += textInsideBtn
-    textOnDisplay = textOnDisplay.value
-
-    if (e.target.id === "clear-btn"){
-        document.querySelector('.display').textContent = ''
-        
-    } else if (e.target.id === 'add-btn'){
-        console.log(textOnDisplay.value)
-        textOnDisplay.textContent = Number(textOnDisplay.innerText) + 2
+    let operand;
+    if (!element.id) {
+        selectedNum += element.innerText
+        display.textContent += selectedNum
+        console.log("nie mam id")
+    } else if (element.id === 'add-btn'){
+        operand = '+'
+        selectedNum = numB
+        display.textContent += ' + '
+    } else if (element.id === 'score-btn'){
+        display.textContent = operate(numA,numB,operand)
     }
 
+    
 }))
+
